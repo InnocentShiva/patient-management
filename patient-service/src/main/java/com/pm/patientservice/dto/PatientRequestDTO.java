843f2349ce41ee9246ce5c765c241786788d7726
InnocentShiva <singh.akshat.solanki@gmail.com>
2025-11-25 20:09:11 +0530

    For creating patient entity in database and handling Exception properly

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientRequestDTO.java
@@ -0,0 +1,74 @@
+package com.pm.patientservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+
+    @NotBlank(message = "Name is required")
+    @Size(max=100, message = "Name can not exceed above 100 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Registered date is required")
+    private String registeredDate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public PatientRequestDTO setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PatientRequestDTO setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public PatientRequestDTO setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public PatientRequestDTO setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+        return this;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public PatientRequestDTO setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+        return this;
+    }
+
+
+
+}

1b45cfb7c1b57d679156fa9b605d8c48b0673858
InnocentShiva <singh.akshat.solanki@gmail.com>
2025-11-29 16:51:53 +0530

    billing microservice is created with one api create account with api written and tested

---
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,33 @@
+package com.pm.billingservice.grpc;
+
+
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//////BillingServiceImplBase
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<billing.BillingResponse> responseObserver){
+        logger.info("createBillingAccount request received {} ", billingRequest.toString());
+
+        // Business logic -
+
+        billing.BillingResponse response = billing.BillingResponse.newBuilder()
+                .setAccountId("123456456")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+
+
+}

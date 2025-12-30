b176cb1a678e07b1a455bf5b8a91d66ddd21538e
InnocentShiva <singh.akshat.solanki@gmail.com>
2025-12-30 10:17:40 +0530

    All the infra changes required to add ECS cluster , domain change , redis , prometheus , grafana transfer on cloud and successfull run of already made apis with production grade seperate settings file

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,17 +1,20 @@
 package com.pm.stack;
 
+
 import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.ecs.Protocol;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.elasticache.CfnCacheCluster;
+import software.amazon.awscdk.services.elasticache.CfnSubnetGroup;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.amazon.awscdk.services.msk.CfnCluster;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
-
+import software.amazon.awscdk.services.servicediscovery.DnsRecordType;
 
 
 import java.util.HashMap;
@@ -24,6 +27,8 @@ public class LocalStack extends Stack{
     private final Vpc vpc;
 
     private final Cluster ecsCluster;
+
+    private final CfnCacheCluster elasticCacheCluster;
 
 
     public LocalStack(final App scope, final String id, final StackProps props){
@@ -54,6 +59,7 @@ public class LocalStack extends Stack{
 //        ECS cluster creation
 
         this.ecsCluster = createEcsCluster();
+        this.elasticCacheCluster = createRedisCluster();
 
 //        Fargate Micro-service creation - auth-service and auth-service-db with auth-service-db to start first and then auth-service.
 
@@ -87,11 +93,12 @@ public class LocalStack extends Stack{
 //
         FargateService patientService =
                 createFargateService("PatientService",
-                        "patient-service",
+                        "patient-service", //Image name and container/service name are same for better mapping of dns-names for cloud service discovery.
                         List.of(4000),
                         patientServiceDb,
                         Map.of(
-                                "BILLING_SERVICE_ADDRESS","host.docker.internal",
+                                "BILLING_SERVICE_ADDRESS","billing-service.patient-management.local",   //This is useful in development - 'host.docker.internal' as internal docker network
+//                                requires the service address convention for dev environment like this. But now for production grade we need to change
                                 "BILLING_SERVICE_GRPC_PORT","9001"
                         ));
 
@@ -100,10 +107,29 @@ public class LocalStack extends Stack{
         patientService.getNode().addDependency(billingService);
 
         patientService.getNode().addDependency(mskCluster);
+        patientService.getNode().addDependency(elasticCacheCluster);
+
 
 //        APIGateway Microservice creation
 
-        createApiGatewayService();
+        ApplicationLoadBalancedFargateService apiGateway =
+                createApiGatewayService();
+
+        apiGateway.getNode().addDependency(elasticCacheCluster);
+
+        //Creating fargate service and specifying the prometheus-prod image to be taken to create task definition
+        FargateService prometheusService = createFargateService(
+            "PrometheusService",
+                "prometheus-prod",
+                List.of(9090),
+                null,
+                null
+        );
+
+//
+        createGrafanaService();
+
+
 
     }
 
@@ -161,6 +187,9 @@ public class LocalStack extends Stack{
 //    CLuster creation function with Service discovery in such a way that no ip address identification is needed and entire mapping
 //    will be done in terms of naming conventions just like in Docker for example -
 //    Address for Auth-service for other internal services will be - auth-service.patient-management.local
+//    For ex- Internally inside docker network billing-service will have a domain name like - billing-service.patient-management.local
+//    What we are trying to establish in this branch is every other service should be able to register themselves with the default CloudNameSpace declared here
+//    so that the naming convention service dsicovery feature can be enabled in aws cloud as well
     private Cluster createEcsCluster(){
         return Cluster.Builder.create(this,"PatientManagementCluster")
                 .vpc(vpc)
@@ -200,6 +229,11 @@ public class LocalStack extends Stack{
 //        Enable any service using this method to link to kafka - brokers if required on these three ports in future
         envVars.put("SPRING_KAFKA_BOOTSTRAP_SERVERS", "localhost.localstack.cloud:4510, localhost.localstack.cloud:4511,localhost.localstack.cloud:4512");
 
+//        Enable fargate service to find where the ecs redis cluster host and port is
+        envVars.put("SPRING_CACHE_TYPE", "redis");
+        envVars.put("SPRING_DATA_REDIS_HOST", elasticCacheCluster.getAttrRedisEndpointAddress());
+        envVars.put("SPRING_DATA_REDIS_PORT", elasticCacheCluster.getAttrRedisEndpointPort());
+
         if(additionalEnvVars != null){
             envVars.putAll(additionalEnvVars);
         }
@@ -229,11 +263,15 @@ public class LocalStack extends Stack{
                 .cluster(ecsCluster)
                 .taskDefinition(taskDefinition)
                 .assignPublicIp(false)
+                .cloudMapOptions(CloudMapOptions.builder()
+                        .name(imageName)
+                        .dnsRecordType(DnsRecordType.A)
+                        .build())
                 .serviceName(imageName)
                 .build();
     }
-
-    private void createApiGatewayService(){
+//      It takes spring cloud gateway , spring boot app and it creates application load balancer and fargate service in order to expose the services in ecs cluster to the world
+    private ApplicationLoadBalancedFargateService createApiGatewayService(){
         FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder.create(this, "APIGatewayTaskDefinition")
                 .cpu(256)
                 .memoryLimitMiB(512)
@@ -244,7 +282,9 @@ public class LocalStack extends Stack{
                         .image(ContainerImage.fromRegistry("api-gateway"))
                         .environment(Map.of(
                                 "SPRING_PROFILES_ACTIVE", "prod",
-                                "AUTH_SERVICE_URL","http://host.docker.internal:4005"
+                                "AUTH_SERVICE_URL","http://auth-service.patient-management.local:4005",  // This url will be changed when shifting from dev region(http://host.docker.internal:4005) to production region
+                                    "REDIS_HOST", elasticCacheCluster.getAttrRedisEndpointAddress(),
+                                "REDIS_PORT",elasticCacheCluster.getAttrRedisEndpointPort()
                         ))
                         .portMappings(List.of(4004).stream()
                                 .map(port -> PortMapping.builder()
@@ -273,9 +313,60 @@ public class LocalStack extends Stack{
                 .taskDefinition(taskDefinition)
                 .desiredCount(1)
                 .healthCheckGracePeriod(Duration.seconds(60))
+                .publicLoadBalancer(true)           //This will enable and attach public Load Balancer which is public to the world and add's up as service to "CloudMap Discovery service" and attached to api-gateway.
+                .cloudMapOptions(CloudMapOptions.builder()
+                        .name("api-gateway")
+                        .dnsRecordType(DnsRecordType.A)  //This line will basically map let's say billing-service.patient-management.local to actual ip address assigned in ecs cluster
+                        .build())
+                .build();
+
+        return apiGateway;
+
+    }
+
+    //    Code for creating elastic cache cluster for local stack mimicking aws
+    private CfnCacheCluster createRedisCluster(){
+        CfnSubnetGroup redisSubnetGroup = CfnSubnetGroup.Builder
+                .create(this, "RedisSubnetGroup")
+                .description("Redis/elasticcache subnet group")
+                .subnetIds(vpc.getPrivateSubnets().stream()
+                        .map(ISubnet::getSubnetId)
+                        .collect(Collectors.toList()))
+                .build();
+
+        return CfnCacheCluster.Builder.create(this,"RedisCluster")
+                .cacheNodeType("cache.t2.micro")
+                .engine("redis")
+                .numCacheNodes(1)
+                .cacheSubnetGroupName(redisSubnetGroup.getCacheSubnetGroupName())
+                .vpcSecurityGroupIds(List.of(vpc.getVpcDefaultSecurityGroup()))
+                .build();
+    }
+
+    private void createGrafanaService(){
+        FargateTaskDefinition taskDefinition = FargateTaskDefinition.Builder
+                .create(this,"GrafanaService")
+                .cpu(256)
+                .memoryLimitMiB(512)
+                .build();
+
+        taskDefinition.addContainer("GrafanaContainer", ContainerDefinitionOptions.builder()
+                        .image(ContainerImage.fromRegistry("grafana/grafana"))
+                        .portMappings(List.of(PortMapping.builder()
+                                        .containerPort(3000) //This is for opening the port for outside ECS cluster
+                                .build()))
+                .build());
+
+        ApplicationLoadBalancedFargateService service = ApplicationLoadBalancedFargateService.Builder
+                .create(this,"GrafanaUIService")
+                .taskDefinition(taskDefinition)
+                .publicLoadBalancer(true)
+                .listenerPort(3000)
+                .desiredCount(1)
                 .build();
 
     }
+
 
     public static void main(final String[] args) {
         App app = new App(AppProps.builder().outdir("./cdk.out").build());
